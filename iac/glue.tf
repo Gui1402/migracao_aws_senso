e5f21ed386116282c34aae101d9068268035f782
Guilherme Lopes <guilherme.lopes@engenharia.ufjf.br>
2021-08-18 15:56:00 -0300

    Nesse comit foram realizadas as seguintes alteracoes
    Criacao de um job para cada tabela migrada
    
    Criacao da esteira de deploy usando github actions

--- iac/glue.tf
+++ iac/glue.tf
@@ -21,3 +21,65 @@ resource "aws_glue_trigger" "trigger_job" {
     job_name = aws_glue_job.transform.name
   }
 }
+
+
+
+resource "aws_glue_crawler" "senso_crowler_docentes" {
+  database_name = "senso"
+  name          = "senso_crowler_docentes"
+  role          = "arn:aws:iam::595877458560:role/service-role/AWSGlueServiceRole-testeigti2"
+
+  s3_target {
+    path = "${var.base_bucket_name}-${var.ambiente}-${var.numero_conta}/staging/senso/docentes/"
+
+  }
+}
+
+
+
+resource "aws_glue_crawler" "senso_crowler_escolas" {
+  database_name = "senso"
+  name          = "senso_crowler_escolas"
+  role          = "arn:aws:iam::595877458560:role/service-role/AWSGlueServiceRole-testeigti2"
+
+  s3_target {
+    path = "${var.base_bucket_name}-${var.ambiente}-${var.numero_conta}/staging/senso/escolas/"
+
+  }
+}
+
+
+resource "aws_glue_crawler" "senso_crowler_gestor" {
+  database_name = "senso"
+  name          = "senso_crowler_gestor"
+  role          = "arn:aws:iam::595877458560:role/service-role/AWSGlueServiceRole-testeigti2"
+
+  s3_target {
+    path = "${var.base_bucket_name}-${var.ambiente}-${var.numero_conta}/staging/senso/gestor/"
+
+  }
+}
+
+
+resource "aws_glue_crawler" "senso_crowler_matricula" {
+  database_name = "senso"
+  name          = "senso_crowler_matricula"
+  role          = "arn:aws:iam::595877458560:role/service-role/AWSGlueServiceRole-testeigti2"
+
+  s3_target {
+    path = "${var.base_bucket_name}-${var.ambiente}-${var.numero_conta}/staging/senso/matricula/"
+
+  }
+}
+
+
+resource "aws_glue_crawler" "senso_crowler_turmas" {
+  database_name = "senso"
+  name          = "senso_crowler_turmas"
+  role          = "arn:aws:iam::595877458560:role/service-role/AWSGlueServiceRole-testeigti2"
+
+  s3_target {
+    path = "${var.base_bucket_name}-${var.ambiente}-${var.numero_conta}/staging/senso/turmas/"
+
+  }
+}
